4ebf9b6cd0ffa4d2b2af9e57e6f26369d82e2c0e
Robert Pirritano <rpirritano@gmail.com>
2020-07-02 16:39:46 -0700

    first commit - completed version 1 of backend

---
+++ src/main/java/com/app/EveryDayMarketApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryDayMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryDayMarketApplication.class, args);
+	}
+
+}
